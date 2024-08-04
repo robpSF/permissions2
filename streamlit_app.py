c5e72715e80ff0636086ff871026de4c48e2d242
robpSF <robert@tstoryteller.com>
2024-08-04 15:28:32 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,29 +16,31 @@ def display_statistics(df):
 def display_filter_section(df, permission_counts):
     st.header('Filter Personas by Permission')
     permission_list = list(permission_counts.keys())
-    selected_permission = st.selectbox('Select a Permission', permission_list)
-    filtered_df = df[df['Permissions'].apply(lambda x: selected_permission in x)]
+    selected_permission = st.selectbox('Select a Permission', permission_list, key='selected_permission')
+    filtered_df = df[df['Permissions'].apply(lambda x: st.session_state.selected_permission in x)]
 
     # Number of personas with this Permission
     num_personas = len(filtered_df)
-    st.write(f"Number of personas with the permission '{selected_permission}': {num_personas}")
+    st.write(f"Number of personas with the permission '{st.session_state.selected_permission}': {num_personas}")
 
     # Permissions also controlling these personas
-    other_permissions = Counter(permission for permissions in filtered_df['Permissions'] for permission in permissions if permission != selected_permission)
+    other_permissions = Counter(permission for permissions in filtered_df['Permissions'] for permission in permissions if permission != st.session_state.selected_permission)
     st.write(f"Permissions also controlling these personas: {', '.join(other_permissions.keys())}")
 
     # Display the table of filtered personas
     st.write(filtered_df[['Name', 'Tags', 'Faction']])
     
-    return df, selected_permission, permission_list
+    return df, st.session_state.selected_permission, permission_list
 
 def edit_permissions(df, permission_list):
     st.header('Edit Permissions')
-    permission_to_replace = st.selectbox('Permission to replace', permission_list)
-    new_permission = st.text_input('New Permission')
+    permission_to_replace = st.selectbox('Permission to replace', permission_list, key='permission_to_replace')
+    new_permission = st.text_input('New Permission', key='new_permission')
     if st.button('Replace'):
-        df['Permissions'] = df['Permissions'].apply(lambda x: [new_permission if p == permission_to_replace else p for p in x])
-        st.success(f'Replaced {permission_to_replace} with {new_permission}')
+        df['Permissions'] = df['Permissions'].apply(lambda x: [st.session_state.new_permission if p == st.session_state.permission_to_replace else p for p in x])
+        st.success(f'Replaced {st.session_state.permission_to_replace} with {st.session_state.new_permission}')
+        # Update the session state with the new DataFrame
+        st.session_state.df = df
         return True
     return False
 
@@ -49,11 +51,13 @@ def main():
     uploaded_file = st.file_uploader("Upload your Excel file", type=["xlsx"])
     
     if uploaded_file:
-        df = load_data(uploaded_file)
+        if 'df' not in st.session_state:
+            st.session_state.df = load_data(uploaded_file)
+        
         while True:
-            permission_counts = display_statistics(df)
-            df, selected_permission, permission_list = display_filter_section(df, permission_counts)
-            if edit_permissions(df, permission_list):
+            permission_counts = display_statistics(st.session_state.df)
+            df, selected_permission, permission_list = display_filter_section(st.session_state.df, permission_counts)
+            if edit_permissions(st.session_state.df, permission_list):
                 continue
             break
 
